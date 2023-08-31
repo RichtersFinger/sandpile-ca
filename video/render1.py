e296b84d7290ae256a3257cbda81ee668f686ec6
RichtersFinger <steffen.finger.privat@gmail.com>
2023-08-31 20:55:40 +0200

    change scaling and positioning of columns

--- video/render1.py
+++ video/render1.py
@@ -8,7 +8,8 @@ from pathlib import Path
 PLATE_WIDTH = 1.43587
 CUBE_REFERENCE_WIDTH = 0.2
 CUBE_REFERENCE_HEIGHT = 15.8519
-PILE_VERTICAL_OFFSET = -0.1
+PILE_VERTICAL_OFFSET = -0.05
+Z_SCALE_FACTOR = 0.8
 
 
 bpy.ops.wm.open_mainfile(filepath="render.blend")
@@ -25,34 +26,47 @@ nx = len(height_array[0])
 ny = len(height_array)
 
 # get column dimensions as
-desired_width = PLATE_WIDTH/max(nx, ny) * 0.9 / math.sqrt(2) / 2
+desired_width = PLATE_WIDTH/max(nx, ny) * 0.98 / 2
 
 # load reference and scale accordingly
 reference_cube = bpy.data.objects["Cube_Reference"]
 reference_cube.location = (0, 0, -2*CUBE_REFERENCE_HEIGHT)
 reference_cube.scale = (desired_width, desired_width, 0.1)
 
-objects = []
-for row in height_array:
+# prepare array to determine which columns to plot
+visible = []
+for ix, row in enumerate(height_array):
     thisrow = []
-    for value in row:
-        column = reference_cube.copy()
-        column.data = reference_cube.data.copy()
-        scn.collection.objects.link(column)
-        thisrow.append(column)
+    for iy, column in enumerate(row):
+        thisrow.append(math.sqrt((nx/2 - ix)**2/nx**2 + (ny/2 - iy)**2/ny**2) < 0.5)
+    visible.append(thisrow)
+
+# generate python objects as copies from reference
+objects = []
+for ix, row in enumerate(height_array):
+    thisrow = []
+    for iy, value in enumerate(row):
+        if visible[ix][iy]:
+            column = reference_cube.copy()
+            column.data = reference_cube.data.copy()
+            scn.collection.objects.link(column)
+            thisrow.append(column)
+        else:
+            thisrow.append(None)
     objects.append(thisrow)
 
 # position columns
 for ix, row in enumerate(objects):
     for iy, column in enumerate(row):
-        column.location = (
-            -desired_width*nx + 2*ix*desired_width,
-            -desired_width*ny + 2*iy*desired_width,
-            -CUBE_REFERENCE_HEIGHT + PILE_VERTICAL_OFFSET \
-                + desired_width*height_array[ix][iy]
-        )
+        if visible[ix][iy]:
+            column.location = (
+                -desired_width*nx + 2*ix*desired_width,
+                -desired_width*ny + 2*iy*desired_width,
+                -CUBE_REFERENCE_HEIGHT + PILE_VERTICAL_OFFSET \
+                    + Z_SCALE_FACTOR * desired_width * height_array[ix][iy]
+            )
 
-bpy.ops.wm.save_as_mainfile(filepath="render_.blend")
+#bpy.ops.wm.save_as_mainfile(filepath="render_.blend")
 
 nframes = 0
 
